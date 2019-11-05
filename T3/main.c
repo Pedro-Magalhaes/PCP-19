3558348b2728d8fdb425b0d93563e93922ab4b8d
costalonga <marcelo.costalonga.cardoso@gmail.com>
2019-11-05 10:58:37 -0300

    Adicao das funcoes e estruturas principais do programa, seguindo o cap 6 do livro

--- T3/main.c
+++ T3/main.c
@@ -3,6 +3,8 @@
 #include <pthread.h>
 // #include <mpi.h>
 #include <string.h>
+#include <sys/sysinfo.h>
+
 
 #define MAX_COST 999999
 #define NO_CITY -1
@@ -14,20 +16,32 @@ typedef int bool;
 typedef struct Tour_Struct {
     int cost;
     int* cities;
-    int num_cities;
+    int count;
 } Tour;
+typedef Tour* tour_t;
 
 typedef struct DEQueue_Struct {
     int head;
     int tail;
-    Tour** list;
+    tour_t* tours;
 }  DEQueue;
+
+typedef struct Stack_Struct {
+    int size;
+    int max_size;
+	tour_t* tours;
+} Stack;
+typedef Stack* my_stack_t
 // -----------------------------------------------------------------------------
 
 // Variaveis Globais -----------------------------------------------------------
-int* digraph;
-Tour* best_tour;
-int n; // number of cities
+int n; // the total number of cities in the problem
+int* digraph; // data structure representing the input digraph
+const int hometown = 0; // data structure representing vertex or city 0, the salesperson’s hometown
+tour_t best_tour; // data structure representing the best tour so far
+
+int* threads_tasks_numbers; // armazena o numero de tasks de cada thread
+int num_cpu;
 // -----------------------------------------------------------------------------
 
 
@@ -48,36 +62,58 @@ int main(int argc, char* argv[]) {
 	read_matrix_file(file_path);
     print_matrix(digraph); //[TODO] delete print
 
+	num_cpu = get_nprocs(); // to get max number of threads in each process
 
-    // [TODO] BFS non-rec
-    // Push_copy(stack, tour); TODO
-    // while (!Empty(&stack)) {
-    while (true) {
-        // curr_tour = Pop(stack);
-        Tour* curr_tour = 0;
-        int City_count = curr_tour->num_cities;
-        if (City_count == n) {
-            printf("[TODO] check best_tour and update"); //[TODO] delete line
-            /* TODO
-            if (Best_tour(curr_tour)) {
-                Update_best_tour(curr_tour);
-            }
-            */
-        } else {
-            for (int nbr = n-1; nbr >= 1; nbr--) {
-                if (Feasible(curr_tour, nbr)) {
-                    Add_city(curr_tour, nbr);
-                    // Push_copy(stack, curr_tour); TODO
-                    Remove_last_city(curr_tour);
-                }
-            }
-        }
-        // Free_tour(curr_tour); [TODO]
-    }
+	// Init best tour
+	best_tour = malloc(sizeof(tour_t));
+	best_tour->cities = malloc(sizeof(int) * (n+1)); // n+1 beacuse last city must be hometown
+	best_tour->count = 0;
+	best_tour->cost = MAX_COST;
 
     free(digraph);
     return 0;
 }
+
+
+void TreeSearch(void* num) {
+
+	// [TODO] BFS non-rec
+	long threadID = (long)num;
+	int num_of_tasks = threads_tasks_numbers[threadID];
+	Push_copy(stack, tour); TODO
+	while (!Empty_stack(&stack)) {
+		tour_t curr_tour = Pop(stack);
+		int City_count = curr_tour->count;
+		if (City_count == n) {
+			if (Best_tour(curr_tour)) {
+				printf("[TODO] create update best tour"); //[TODO] delete line
+				// Update_best_tour(curr_tour);
+			}
+		} else {
+			for (int nbr = n-1; nbr >= 1; nbr--) {
+				if (Feasible(curr_tour, nbr)) {
+					Add_city(curr_tour, nbr);
+					Push_copy(stack, curr_tour);
+					Remove_last_city(curr_tour);
+				}
+			}
+		}
+		Free_tour(curr_tour);
+	}
+}
+
+
+void set_tasks(my_stack_t stack) {
+	threads_tasks_numbers = malloc(num_cpu * sizeof(int));
+	int num_of_tasks = (stack->size)/num_cpu;
+	for(int i = 0; i < num_cpu; i++) {
+		threads_tasks_numbers[i] = num_of_tasks;
+	}
+	for(int i=0; i < ((stack->size)%num_cpu); i++) {
+		threads_tasks_numbers[i]++;
+	}
+}
+
 
 void read_matrix_file(char* file_path) {
     int i, j;
@@ -86,15 +122,14 @@ void read_matrix_file(char* file_path) {
 		fprintf(stderr, "[Error] Null file: %s\n", file_path);
         exit(-1);
 	}
-
     digraph = malloc(n*n*sizeof(int));
     for (i = 0; i < n; i++) {
         for (j = 0; j < n; j++) {
             int offset = i*n+j;
             fscanf(matrix_file, "%d", &digraph[offset]);
             if (i == j && digraph[offset] != 0) {
-              fprintf(stderr, "[Error] Index matrix[%d][%d] must be 0\n");
-              exit(-1);
+            	fprintf(stderr, "[Error] Index matrix[%d][%d] must be 0\n");
+              	exit(-1);
             }
         }
     }
@@ -117,9 +152,9 @@ void print_matrix(int* digraph) {
     visited, and, if not, whether it can possibly lead to a least-cost tour. If
     the city is feasible, we add it to the tour, and call Depth first search
 */
-bool Feasible(Tour* tour, int city) {
-    int last_city = (tour->cities[(tour->num_cities)-1]);
-    for (int i = 0; i < tour->num_cities; i++) {
+bool Feasible(tour_t tour, int city) {
+    int last_city = (tour->cities[(tour->count)-1]);
+    for (int i = 0; i < tour->count; i++) {
         if (tour->cities[i] == city ) {
             // ja foi visitado
             return false;
@@ -133,18 +168,18 @@ bool Feasible(Tour* tour, int city) {
     }
 }
 
-void Add_city(Tour* tour, int city) {
-    int tmp_last_city = tour->cities[(tour->num_cities)-1];
-    tour->cities[tour->num_cities] = city;
-    (tour->num_cities)++;
+void Add_city(tour_t tour, int city) {
+    int tmp_last_city = tour->cities[(tour->count)-1];
+    tour->cities[tour->count] = city;
+    (tour->count)++;
     tour->cost = tour->cost + compute_cost(tmp_last_city, city);
 }
 
-void Remove_last_city(Tour* tour) {
+void Remove_last_city(tour_t tour) {
     int curr_last_city, tmp_last_city;
     tmp_last_city = get_last_city(tour);
-    tour->cities[tour->num_cities-1] = NO_CITY;
-    (tour->num_cities)--;
+    tour->cities[tour->count-1] = NO_CITY;
+    (tour->count)--;
     curr_last_city = get_last_city(tour);
     tour->cost = tour->cost - compute_cost(curr_last_city, tmp_last_city);
 }
@@ -155,6 +190,93 @@ int compute_cost(int c1, int c2) {
     return digraph[offset];
 }
 
-int get_last_city(Tour* t) {
-    return t->cities[(t->num_cities)-1];
+int get_last_city(tour_t t) {
+    return t->cities[(t->count)-1];
 }
+
+// Pag 305 - cap 6.2.3 - Data structures for the serial implementations
+/* Find the ith city on the partial tour */
+int Tour_city(tour_t tour, int i) {
+	return tour->cities[i];
+	//[TODO] try macro later:
+	// #define Tour_city(tour, i) (tour->cities[i])
+}
+
+tour_t Pop(my_stack_t stack) {
+    if (Empty_stack(stack)) {
+		printf("[Error - Pop]: Stack is empty\n");
+        exit(-1);
+    }
+	int offset = (stack->size) - 1;
+    tour_t tmp = stack->tours[offset];
+    stack->tours[offset] = NULL;
+    (stack->size)--;
+    return tmp;
+}
+
+void Push(my_stack_t stack, int city) {
+	int loc = stack->size;
+    if (stack->max_size == stack->size) {
+        printf("[Error - Push]: Stack is already full\n");
+        exit(-1);
+    }
+    stack->tours[loc] = city;
+    (stack->size)++;
+}
+
+void Push_copy(my_stack_t stack, tour_t tour) {
+	int loc = stack->size;
+    if (stack->max_size == stack->size) {
+		printf("[Error - Push_copy]: Stack is already full\n");
+        exit(-1);
+    }
+    tour_t tmp = Alloc_tour();
+    Copy_tour(tour, tmp);
+    stack->tours[loc] = tmp;
+    (stack->size)++;
+}
+
+tour_t Alloc_tour() {
+	tour_t t = malloc(sizeof(tour_t));
+	t->cities = malloc(sizeof(int) * (n+1)); // n+1 beacuse last city must be hometown
+	return t;
+}
+
+bool Empty_stack(my_stack_t stack) {
+    return stack->size == 0 ? true : false;
+}
+
+void Free_tour(tour_t tour) {
+	free(tour->cities);
+	free(tour);
+}
+
+bool Best_tour(tour_t tour) {
+
+	/* Create function that checks MPI messages to look for the best tour across all processes */
+    // check_best_tours(); [TODO]
+
+    int last_city = get_last_city(tour);
+	int current_cost = tour->cost;
+    int new_cost = tour->cost + compute_cost(last_city, hometown);
+    return (newCost < best_tour->cost ? true : false);
+}
+
+// [TODO] MPI functions Receive/Send (pag 330)
+/*
+void Send tour(tour t tour, int dest) {
+	int position = 0;
+	MPI Pack(tour->cities, n+1, MPI INT, contig buf, LARGE, &position, comm);
+	MPI Pack(&tour->count, 1, MPI INT, contig buf, LARGE, &position, comm);
+	MPI Pack(&tour->cost, 1, MPI INT, contig buf, LARGE, &position, comm);
+	MPI Send(contig buf, position, MPI PACKED, dest, 0, comm);
+}
+
+void Receive tour(tour t tour, int src) {
+	int position = 0;
+	MPI Recv(contig buf, LARGE, MPI PACKED, src, 0, comm, MPI STATUS IGNORE);
+	MPI Unpack(contig buf, LARGE, &position, tour->cities, n+1, MPI INT, comm);
+	MPI Unpack(contig buf, LARGE, &position, &tour->count, 1, MPI INT, comm);
+	MPI Unpack(contig buf, LARGE, &position, &tour->cost, 1, MPI INT, comm);
+}
+*/
--- T3/main.h
+++ T3/main.h
@@ -3,10 +3,22 @@
 void read_matrix_file(char* file_path);
 void print_matrix(int* digraph);
 int compute_cost(int c1, int c2);
-int get_last_city(Tour* t);
+int get_last_city(tour_t t);
 
-bool Feasible(Tour* tour, int city);
-void Add_city(Tour* tour, int city);
-void Remove_last_city(Tour* tour);
+bool Feasible(tour_t tour, int city);
+void Add_city(tour_t tour, int city);
+void Remove_last_city(tour_t tour);
 void Push_copy(); //TODO
+
+void set_tasks(my_stack_t stack);
+void TreeSearch(void* num);
+int Tour_city(tour_t tour, int i);
+tour_t Pop(my_stack_t stack);
+void Push(my_stack_t stack, int city);
+void Push_copy(my_stack_t stack, tour_t tour);
+tour_t Alloc_tour();
+bool Empty_stack(my_stack_t stack);
+void Free_tour(tour_t tour);
+bool Best_tour(tour_t tour);
+
 // -----------------------------------------------------------------------------
