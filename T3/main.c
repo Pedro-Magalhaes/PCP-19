f49ff718c76d3ab42dcf40a01cbaec12acb0dbf7
costalonga <marcelo.costalonga.cardoso@gmail.com>
2019-11-08 16:18:38 -0500

    Pthreads solution v1

--- T3/main.c
+++ T3/main.c
@@ -26,6 +26,7 @@ typedef struct DEQueue_Struct {
     int tail;
     tour_t* tours;
     int max_size;
+    bool full;
 }  DEQueue;
 
 typedef struct Stack_Struct {
@@ -58,8 +59,27 @@ const int TOUR_TAG = 1;
 // const int TOUR_TAG = 3;
 // -----------------------------------------------------------------------------
 
-// Macros ----------------------------------------------------------------------
-// #define Tour_cost(tour) (tour->cost)
+// Why? ----------------------------------------------------------------------
+typedef struct barrier_struct {
+   int curr_tc;  // Number of threads that have entered the barrier
+   int max_tc;   // Number of threads that need to enter the barrier
+   pthread_mutex_t mutex;
+   pthread_cond_t ok_to_go;
+}  Barrier;
+
+
+my_barrier_t bar_str;
+int init_tour_count;
+int queue_size;
+
+my_queue_t queue;
+
+
+// "Macros" --------------------------------------------------------------------
+int Tour_cost(tour_t tour) { return (tour->cost); }
+int Tour_count(tour_t t) { return t->count; }
+// -----------------------------------------------------------------------------
+
 // -----------------------------------------------------------------------------
 
 int main(int argc, char* argv[]) {
@@ -90,8 +110,8 @@ int main(int argc, char* argv[]) {
 	read_matrix_file(file_path);
     // print_matrix(digraph); //[TODO] delete print
 
-    // thread_count = get_nprocs(); // to get max number of threads in each process
-    thread_count = 4; // to get max number of threads in each process
+    // thread_count = get_nprocs(); [TODO] -use it // to get max number of threads in each process
+    thread_count = strtol(argv[3], NULL, 4);
     printf("Number of threads: %d\n\n", thread_count);
 
 	// Init best tour
@@ -101,6 +121,7 @@ int main(int argc, char* argv[]) {
     best_tour_cost = Tour_cost(best_tour);
 
     thread_handles = malloc(thread_count*sizeof(pthread_t));
+    bar_str = My_barrier_init(thread_count);
     // bar_str = My_barrier_init(thread_count);
     // pthread_mutex_init(&best_tour_mutex, NULL);
     // Init_term();
@@ -127,16 +148,23 @@ int main(int argc, char* argv[]) {
    free(threads_tasks_numbers);
    free(thread_handles);
    free(digraph);
+   My_barrier_destroy(bar_str);
+   pthread_mutex_destroy(&best_tour_mutex);
    return 0;
 }
 
 
-void* Thread_tree_search(void* num) {
-	long id = (long)num;
+void* Thread_tree_search(void* rank) {
+	long id = (long)rank;
 	// int num_of_tasks = threads_tasks_numbers[id]; //[TODO]
     tour_t curr_tour;
 
     my_stack_t stack = Init_stack(); //[TODO] Get right stack
+
+    Print_stack(stack, id, "Init stack"); // del print
+
+    Partition_tree(id, stack);
+
     //  CALL Partition_tree(id, stack); [TODO] How to make Partition_tree
     // ... so we can assign each thread its initial collection of subtrees
     // OR my_stack_t my_stack_thread = pickElementsFromStack(my_stack, numThread, myCount); ??
@@ -145,8 +173,7 @@ void* Thread_tree_search(void* num) {
     // DFS
     while (!Empty_stack(stack)) {
 		curr_tour = Pop(stack);
-		int City_count = curr_tour->count;
-		if (City_count == n) {
+		if (Tour_count(curr_tour) == n) {
 			if (Best_tour(curr_tour)) {
 				Update_best_tour(curr_tour);
 			}
@@ -392,9 +419,20 @@ void Print_tour(tour_t tour) {
 }
 
 
-int Tour_cost(tour_t tour) {
-    return (tour->cost);
+void Print_stack(my_stack_t stack, long pthrdID, char* title) {
+    // printf("(Rank %ld) - thread (%ld) - %s\n", cluster_rank, pthrdID, title);  //[later]
+    printf("Thread [%ld] - %s\n", pthrdID, title);
+    for (int i = 0; i < stack->size; i++) {
+        int sz = stack->tours[i]->count;
+        for (int j = 0; j < sz-1; j++) {
+            printf("%d -> ", stack->tours[i]->cities[j]);
+        }
+        printf("%d\n\n", stack->tours[i]->cities[sz-1]);
+    }
 }
+
+
+
 
 // [TODO] MPI functions Receive/Send (pag 330)
 /*
@@ -413,4 +451,234 @@ void Receive tour(tour t tour, int src) {
 	MPI Unpack(contig buf, LARGE, &position, &tour->count, 1, MPI INT, comm);
 	MPI Unpack(contig buf, LARGE, &position, &tour->cost, 1, MPI INT, comm);
 }
+
+void multiThreadTreeSearch(void)  // [HERE]
+{
+    my_stack = Init_stack();
+    getMyStack(my_stack); // Aqui, a stack vai ter pelo menos nThreads tarefas ao invés de pelo menos 1
+
+	setNumberOfTasks(my_stack); // vai calcular o numero de tarefas que cada thread tem que pegar da stack
+
+	// Criacao das threads e inicializacao do semaforo para a my_stack (do rango)
+	sem_init(&sem_stack, 0, 1);     // sem_stack vai ter valor = 1
+	int i;
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+	pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM);
+
+	pthread_t* threads;
+	threads = malloc(sizeof(pthread_t) * nThreads);
+
+	// cria as threads executadoras
+	for (i = 0; i < thread_count; i++)
+	{
+		//pthread_create(&threads[i], &attr,TreeSearch, (void*)threadInfos[i]);
+		pthread_create(&threads[i], &attr, Thread_tree_search, (void*)i);
+	}
+
+	// espera até as threads terminarem
+	for (i = 0; i < thread_count; i++)
+	{
+		pthread_join(threads[i], NULL);
+	}
+
+    freeStack(my_stack);
+}
 */
+
+void Partition_tree(long my_rank, my_stack_t stack) {
+   int my_first_tour, my_last_tour, i;
+
+   if (my_rank == 0) queue_size = Get_upper_bd_queue_sz();
+   // My_barrier(bar_str);
+// # ifdef DEBUG
+   printf("Th %ld > queue_size = %d\n", my_rank, queue_size);
+// # endif
+   if (queue_size == 0) pthread_exit(NULL);
+
+   if (my_rank == 0) Build_initial_queue();
+   // My_barrier(bar_str);
+   Set_init_tours(my_rank, &my_first_tour, &my_last_tour);
+// # ifdef DEBUG
+   printf("Th %ld > init_tour_count = %d, first = %d, last = %d\n",
+         my_rank, init_tour_count, my_first_tour, my_last_tour);
+// # endif
+   for (i = my_last_tour; i >= my_first_tour; i--) {
+// # ifdef DEBUG
+      Print_tour(Queue_elt(queue,i));
+// # endif
+      Push(stack, Queue_elt(queue,i));
+   }
+// # ifdef PTSDEBUG
+   Print_stack(stack, my_rank, "After set up");
+// # endif
+
+}
+
+int Get_upper_bd_queue_sz(void) {
+   int fact = n-1;
+   int size = n-1;
+
+   while (size < thread_count) {
+      fact++;
+      size *= fact;
+   }
+   return size;
+}  /* Get_upper_bd_queue_sz */
+
+// void My_barrier(my_barrier_t bar) {
+//    pthread_mutex_lock(&bar->mutex);
+//    bar->curr_tc++;
+//    if (bar->curr_tc == bar->max_tc) {
+//       bar->curr_tc = 0;
+//       pthread_cond_broadcast(&bar->ok_to_go);
+//    } else {
+//       // Wait unlocks mutex and puts thread to sleep.
+//       //    Put wait in while loop in case some other
+//       // event awakens thread.
+//       while (pthread_cond_wait(&bar->ok_to_go, &bar->mutex) != 0);
+//       // Mutex is relocked at this point.
+//    }
+//    pthread_mutex_unlock(&bar->mutex);
+// }  /* My_barrier */
+
+void Build_initial_queue(void) {
+   int curr_sz = 0;
+   int nbr;
+   tour_t tour = Alloc_tour(NULL);
+
+   Init_tour(tour, 0);
+   queue = Init_queue(2*queue_size);
+
+   /* Breadth-first search */
+   Push_Bottom_DEQ(queue, tour);  // Enqueues a copy
+// printf("Freeing %p\n", tour);
+   Free_tour(tour);
+   curr_sz++;
+   while (curr_sz < thread_count) {
+      tour = Pop_Top_DEQ(queue);
+//    printf("Dequeued %p\n", tour);
+      curr_sz--;
+      for (nbr = 1; nbr < n; nbr++)
+         if (!wasVisited(tour, nbr)) {
+            Add_city(tour, nbr);
+            Push_Bottom_DEQ(queue, tour);
+            curr_sz++;
+            Remove_last_city(tour);
+         }
+//    printf("Freeing %p\n", tour);
+      Free_tour(tour);
+   }  /* while */
+   init_tour_count = curr_sz;
+
+// # ifdef DEBUG
+   // Print_queue(queue, 0, "Initial queue");
+// # endif
+}  /* Build_initial_queue */
+
+
+my_barrier_t My_barrier_init(int thr_count) {
+   my_barrier_t bar = malloc(sizeof(Barrier));
+   bar->curr_tc = 0;
+   bar->max_tc = thr_count;
+   pthread_mutex_init(&bar->mutex, NULL);
+   pthread_cond_init(&bar->ok_to_go, NULL);
+
+   return bar;
+}  /* My_barrier_init */
+
+void My_barrier_destroy(my_barrier_t bar) {
+   pthread_mutex_destroy(&bar->mutex);
+   pthread_cond_destroy(&bar->ok_to_go);
+   free(bar);
+}  /* My_barrier_destroy */
+
+
+void Set_init_tours(long my_rank, int* my_first_tour_p, int* my_last_tour_p) {
+   int quotient, remainder, my_count;
+
+   quotient = init_tour_count/thread_count;
+   remainder = init_tour_count % thread_count;
+   if (my_rank < remainder) {
+      my_count = quotient+1;
+      *my_first_tour_p = my_rank*my_count;
+   } else {
+      my_count = quotient;
+      *my_first_tour_p = my_rank*my_count + remainder;
+   }
+   *my_last_tour_p = *my_first_tour_p + my_count - 1;
+}   /* Set_init_tours */
+
+
+tour_t Queue_elt(my_queue_t q, int i) {
+    return (q->tours[(q->head + (i)) % q->max_size]);
+}
+
+// ----------------------------------------------------------------------------
+
+void Init_tour(tour_t tour, int cost) {
+    tour->cities[0] = 0;
+    for (int i = 1; i <= n; i++) {
+        tour->cities[i] = NO_CITY;
+    }
+    tour->cost = cost;
+    tour->count = 1;
+}
+
+my_queue_t Init_queue(int size) {
+   my_queue_t new_queue = malloc(sizeof(DEQueue));
+   new_queue->tours = malloc(size*sizeof(tour_t));
+   new_queue->max_size = size;
+   new_queue->head = new_queue->tail = new_queue->full = 0;
+   return new_queue;
+}
+
+// Remove the tour at the head of the queue
+tour_t Pop_Top_DEQ(my_queue_t queue) {
+   tour_t tmp;
+   if (isQueueEmpty(queue)) {
+      fprintf(stderr, "Attempting to dequeue from empty queue\n");
+      exit(-1);
+   }
+   tmp = queue->tours[queue->head];
+   queue->head = (queue->head + 1) % queue->max_size;
+   return tmp;
+}
+
+// Add a new tour to the tail of the queue
+void Push_Bottom_DEQ(my_queue_t queue, tour_t tour) {
+  tour_t tmp;
+  if (queue->full == true) {
+     fprintf(stderr, "Attempting to enqueue a full queue\n");
+     fprintf(stderr, "Max_size = %d, head = %d, tail = %d\n", \
+             queue->max_size, queue->head, queue->tail);
+     exit(-1);
+  }
+  tmp = Alloc_tour(NULL);
+  Copy_tour(tour, tmp);
+  queue->tours[queue->tail] = tmp;
+  queue->tail = (queue->tail + 1) % queue->max_size;
+  if (queue->tail == queue->head)
+     queue->full = true;
+}
+
+bool wasVisited(tour_t tour, int city) {
+    for(int i = 0; i < Tour_count(tour); i++) {
+        if (Tour_city(tour,i) == city ) {
+            return true;
+        }
+    }
+   return false;
+}
+
+bool isQueueEmpty(my_queue_t queue) {
+    if (queue->full == true) {
+        return false;
+    }
+    else if (queue->head != queue->tail) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
--- T3/main.h
+++ T3/main.h
@@ -5,6 +5,10 @@ typedef struct Stack_Struct Stack;
 typedef Stack* my_stack_t;
 typedef int bool;
 
+typedef struct barrier_struct Barrier;
+typedef Barrier* my_barrier_t;
+typedef DEQueue* my_queue_t;
+
 // Funcoes ---------------------------------------------------------------------
 void read_matrix_file(char* file_path);
 void print_matrix(int* digraph);
@@ -18,13 +22,14 @@ void Push_copy(); //TODO
 void Print_tour(tour_t tour);
 
 void set_tasks(my_stack_t stack);
-void* Thread_tree_search(void* num);
+void* Thread_tree_search(void* rank);
 int Tour_city(tour_t tour, int i);
 tour_t Pop(my_stack_t stack);
 void Push(my_stack_t stack, tour_t tour);
 void Push_copy(my_stack_t stack, tour_t tour);
 tour_t Alloc_tour();
 int Tour_cost(tour_t tour);
+int Tour_count(tour_t t);
 
 bool Empty_stack(my_stack_t stack);
 my_stack_t Init_stack(void);
@@ -36,4 +41,25 @@ void Copy_tour(tour_t tour1, tour_t tour2);
 void Update_best_tour(tour_t tour);
 void Broeadcast_best_cost_to_all(int tour_cost);
 
+void Print_stack(my_stack_t stack, long pthrdID, char* title);
 // -----------------------------------------------------------------------------
+
+
+void Set_init_tours(long my_rank, int* my_first_tour_p, int* my_last_tour_p);
+void My_barrier_destroy(my_barrier_t bar);
+my_barrier_t My_barrier_init(int thr_count);
+void Build_initial_queue(void);
+int Get_upper_bd_queue_sz(void);
+tour_t Queue_elt(my_queue_t q, int i);
+void Partition_tree(long my_rank, my_stack_t stack);
+    // Init_tour();
+    // Init_queue();
+    // Pop_Top_DEQ();
+    // Push_Bottom_DEQ();
+    // Print_queue();
+void Init_tour(tour_t tour, int cost);
+my_queue_t Init_queue(int size);
+void Push_Bottom_DEQ(my_queue_t queue, tour_t tour);
+tour_t Pop_Top_DEQ(my_queue_t queue);
+bool wasVisited(tour_t tour, int city);
+bool isQueueEmpty(my_queue_t queue);
